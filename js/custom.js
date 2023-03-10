688d1114bca7154815d47b2eee20273d9527f415
Shiam <siam.hossain1994@gmail.com>
2023-03-10 18:32:50 +0600

    Add course-intro, team, counter section

--- js/custom.js
+++ js/custom.js
@@ -399,35 +399,6 @@
     });
     wow.init();
 
-    // $("#main-menu ul a")    
-    // .click(function(e) {
-    //     var link = $(this);
-
-    //     var item = link.parent("li");
-        
-    //     if (item.hasClass("active")) {
-    //         item.removeClass("active").children("a").removeClass("active");
-    //     } else {
-    //         item.addClass("active").children("a").addClass("active");
-    //     }
-
-    //     if (item.children("ul").length > 0) {
-    //         var href = link.attr("href");
-    //         link.attr("href", "#");
-    //         setTimeout(function () { 
-    //             link.attr("href", href);
-    //         }, 300);
-    //         e.preventDefault();
-    //     }
-    // })
-    // .each(function() {
-    //     var link = $(this);
-    //     if (link.get(0).href === location.href) {
-    //         link.addClass("active").parents("li").addClass("active");
-    //         return false;
-    //     }
-    // });
-
     // init Isotope
     $('.course-category-items').isotope({
         itemSelector: '.item',
